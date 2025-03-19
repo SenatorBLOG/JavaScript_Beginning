4dac649b049bac5fbffaa915ce1931d58a5d514d
SenatorBLOG <87308663+SenatorBLOG@users.noreply.github.com>
2025-03-19 16:46:55 -0700

    variables

---
+++ SecondPractice/mycode.js
@@ -0,0 +1,22 @@
+//JavaScript source code
+let elm1 = document.getElementById("input1");
+let elm2 = document.getElementById("input2");
+
+function add(){
+    //variables
+    "us strict";
+    let result = document.getElementById("output");
+
+    result.innerText = +elm1.value + +elm2.value;
+    result.style.color = "red";
+
+    /*document.getElementById("output").innerText = 
+    Number(document.getElementById("input1").value)
+     + +document.getElementById("input2").value;*/
+}
+function multiply(){
+    let result = document.getElementById("output");
+
+    result.innerText = elm1.value * elm2.value;
+    result.style.color = "red";
+}
